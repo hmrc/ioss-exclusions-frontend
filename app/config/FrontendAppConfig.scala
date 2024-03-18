06b58fadaf0a3ceeb7affaa1ca330d8f61410296
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2024-03-18 12:16:39 +0000

    Alter the way feedback url loads (#31)

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -39,8 +39,9 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val signOutUrl: String       = configuration.get[String]("urls.signOut")
   val iossYourAccountUrl: String = configuration.get[String]("urls.yourAccountUrl")
 
-  private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
-  val exitSurveyUrl: String             = s"${exitSurveyBaseUrl}${origin.toLowerCase}"
+  private val exitSurveyBaseUrl: String = configuration.get[String]("microservice.services.feedback-frontend.host") +
+    configuration.get[String]("microservice.services.feedback-frontend.basePath")
+  val exitSurveyUrl: String             = s"${exitSurveyBaseUrl}/${origin.toLowerCase}"
 
   val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("features.welsh-translation")
